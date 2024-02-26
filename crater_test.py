4c980a31e6a2d5ba4dae114dc3b7c464a8f4f42c
Ojojostar <josephtang26@gmail.com>
2024-02-25 21:01:52 -0500

    first git

---
+++ crater_test.py
@@ -0,0 +1,40 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     formats: py:light,ipynb
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.15.2
+# ---
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sns
+from sklearn.decomposition import PCA
+# from sklearn.feature_selection import mutual_info_regression
+# from sklearn.python import cross_val_score
+# from xgboost import XGBRegressor
+
+def apply_pca(X, standardize=True):
+    # Standardize
+    if standardize:
+        X = (X - X.mean(axis=0)) / X.std(axis=0)
+    # Create principal components
+    pca = PCA()
+    X_pca = pca.fit_transform(X)
+    # Convert to dataframe
+    component_names = [f"PC{i+1}" for i in range(X_pca.shape[1])]
+    X_pca = pd.DataFrame(X_pca, columns=component_names)
+    # Create loadings
+    loadings = pd.DataFrame(
+        pca.components_.T,  # transpose the matrix of loadings
+        columns=component_names,  # so the columns are the principal components
+        index=X.columns,  # and the rows are the original features
+    )
+    return pca, X_pca, loadings
+
+df = pd.read_csv(r"C:\Users\josep\Documents\Joseph\crater\M3_data.csv")
